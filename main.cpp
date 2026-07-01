45208a52e1a6efb5b0593d60d03122ab231b346c
Darwish-mmu <MUHAMMAD.DARWISH.AHMAD@student.mmu.edu.my>
2026-07-01 16:33:40 +0800

    Created flashcard console interface in main function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,53 @@
-#include <iostream>
-#include <string>
+#include <iostream>  
+#include <string>     
+
 using namespace std;
 
-int main () {
-    cout << "Initial code skeleton";
+void print(string str) {
+    cout << str << endl;
+}
+
+int main() {
+    int choice = 0;
+
+    do {
+        cout << endl;
+        print("---------------------------------");
+        print("      FLASHCARD CONSOLE          ");
+        print("---------------------------------");
+        print("1. Add FlashCard");
+        print("2. Remove FlashCard");
+        print("3. Edit Flashcards");
+        print("4. Exit");
+        cout << endl;
+
+        cout << "Enter your choice (number): ";
+        cin >> choice;
+        cin.ignore(); 
+        cout << endl;
+
+        if (choice == 1) {
+            print ("----------");
+        }
+
+        else if (choice == 2) {
+            print ("----------");
+        }
+
+        else if (choice == 3) {
+            print ("----------");
+        }
+
+        else if (choice == 4) {
+            print("Goodbye!");
+            return 0;
+        }
+
+        else {
+            print("Invalid choice. Please select 1-5.");
+        }
+    
+    } while (true);
+
     return 0;
 }
